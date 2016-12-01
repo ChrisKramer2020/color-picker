1fb0794d600dcb23eff424864fc415407feb294f
Christopher Kramer <Hendrix1957@gmail.com>
2016-12-01 13:01:34 -0500

    my work

--- public/main.js
+++ public/main.js
@@ -1,6 +1,48 @@
+let hue = 360
+let saturation = 100
+let lightness = 50
+let alpha = 0
+
+const getHSL = () => {
+  return `hsl(${hue}, ${saturation}%, ${lightness}%)`
+}
+
 const main = () => {
-  document.getElementById('root').textContent = 'Hello, World!'
+  const colorbox = document.querySelector('div.colorbox')
+  const hueInput = document.querySelector('input[name=a]')
+  hueInput.addEventListener('input', () => {
+    hue = hueInput.value
+    colorbox.style.backgroundColor = getHSL()
+    document.querySelector('h3').textContent = getHSL()
+    console.log(getHSL())
+    console.log(`hsl(${hue}, ${saturation}%, ${lightness}%)`)
+  })
+
+  const saturationInput = document.querySelector('input[name=b]')
+  saturationInput.addEventListener('input', () => {
+    saturation = saturationInput.value
+    colorbox.style.backgroundColor = getHSL()
+    document.querySelector('h3').textContent = getHSL()
+    console.log(getHSL())
+    console.log(`hsl(${hue}, ${saturation}%, ${lightness}%)`)
+  })
+
+  const lightnessInput = document.querySelector('input[name=c]')
+  lightnessInput.addEventListener('input', (event) => {
+    lightness = lightnessInput.value
+    colorbox.style.backgroundColor = getHSL()
+    document.querySelector('h3').textContent = getHSL()
+    console.log(`hsl(${hue}, ${saturation}%, ${lightness}%)`)
+    console.log(getHSL())
+  })
+  const alphaInput = document.querySelector('input[name=d]')
+  alphaInput.addEventListener('input', (event) => {
+    alpha = alphaInput.value
+    colorbox.style.backgroundColor = getHSL()
+    document.querySelector('h3').textContent = getHSL()
+    console.log(`hsl(${alpha}, ${saturation}%, ${lightness}%))`)
+    console.log(getHSL())
+  })
 }
 
 document.addEventListener('DOMContentLoaded', main)
-
